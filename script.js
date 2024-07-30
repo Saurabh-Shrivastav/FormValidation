238ce43e871114b49463a46e1f7e685afba82060
Saurabh-Shrivastav <saurabhshrivastav0345@gmail.com>
2024-07-30 09:04:35 +0530

    commit

---
+++ script.js
@@ -0,0 +1,85 @@
+let nameError    = document.getElementById("name-error")
+let phoneError   = document.getElementById("phone-error")
+let emailError   = document.getElementById("email-error")
+let messageError = document.getElementById("message-error")
+let submitError  = document.getElementById("submit-error")
+
+// Validation for Name.
+function validateName() {
+    let name =document.getElementById("contact-name").value;
+    
+    if (name.length == 0) {
+        nameError.innerHTML = 'Name is required';
+        return false;
+    }
+    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+        nameError.innerHTML = 'Write full name';  
+        return false;
+    }
+    nameError.innerHTML = '<i class="fa-solid fa-circle-check fa-bounce"></i> valid';
+    return true;
+}
+
+// Validation for Phone.
+function validatePhone() {
+    let phone = document.getElementById("contact-phone").value;
+    
+    if (phone.length == 0) {
+        phoneError.innerHTML = 'Phone no is required';
+        return false;
+    }
+    if (phone.length !== 10) {
+        phoneError.innerHTML = 'Phone no should be 10 digits';
+        return false;
+    }
+    if (!phone.match(/^[0-9]{10}$/)) {
+        phoneError.innerHTML = 'Only digits please.'
+        return false;
+    } 
+    
+    phoneError.innerHTML = '<i class="fa-solid fa-circle-check fa-bounce"></i> valid';
+    return true;
+}
+
+// Validation for Email.
+function validateEmail() {
+    let email = document.getElementById("contact-email").value;
+    
+    if (email.length == 0) {
+        emailError.innerHTML = 'Email is required'
+        return false;
+    }
+    if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+        emailError.innerHTML = 'Email Invalid'
+        return false;
+    }
+    
+    emailError.innerHTML = '<i class="fa-solid fa-circle-check fa-bounce"></i> valid';
+    return true;
+}
+
+// Validation for Message box.
+function validateMessage(){
+    let message = document.getElementById("contact-message").value;
+    let required = 30;
+    let left = required - message.length;
+    
+    if (left>0) {
+        messageError.innerHTML = left + 'more characters require'
+        return false;
+    }
+    messageError.innerHTML = '<i class="fa-solid fa-circle-check fa-bounce"></i> valid';
+    return true;
+}
+
+// Validation for submit button.
+function validateForm() {
+    if (!validateName() || !validatePhone() || !validateEmail() || !validateMessage()) {
+        submitError.style.display = 'block';
+        submitError.innerHTML = "Please fix error to submit";
+        setTimeout(function () {
+            submitError.style.display = 'none';
+        }, 3000)
+        return false;
+    }   
+}
